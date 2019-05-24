ccf9281806b7c1f8bf5d9e264608f3978ad1b5b0
JoshCrosby <joshdcrosby@gmail.com>
2019-05-24 16:47:36 -0600

    Initial commit

---
+++ script.py
@@ -0,0 +1,15 @@
+import sys
+
+print(sys.version)
+print(sys.executable)
+
+name = input("Your Name? ")
+print("Hello,", name)
+# def greet(who_to_greet):
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
+
+
+# print(greet("World"))
+# print(greet("Josh"))
+
